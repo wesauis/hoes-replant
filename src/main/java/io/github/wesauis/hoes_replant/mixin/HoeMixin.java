70eb0188474f200ca466d2b70d2af2693fcb2af4
Wesley Augusto Isotton <wesleya.isotton@gmail.com>
2022-04-17 20:40:44 -0300

    detect when a crop is broken by a hoe

---
+++ src/main/java/io/github/wesauis/hoes_replant/mixin/HoeMixin.java
@@ -0,0 +1,41 @@
+package io.github.wesauis.hoes_replant.mixin;
+
+import io.github.wesauis.hoes_replant.extension.LogAware;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MiningToolItem.class)
+public abstract class HoeMixin implements LogAware {
+
+    private static final TagKey<Block> TAG_CROPS = BlockTags.CROPS;
+
+    @Inject(method = "postMine", at = @At("HEAD"))
+    private void blockMined(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner, CallbackInfoReturnable<Boolean> cir) {
+        if (world.isClient) return;
+        if (!isHoe()) return;
+        if (!state.isIn(TAG_CROPS)) return;
+
+        cropHarvested(stack, world, state, pos, miner, cir);
+    }
+
+    private boolean isHoe() {
+        return HoeItem.class.getName().equals(getClass().getName());
+    }
+
+}
